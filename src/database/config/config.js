351d55c6fc28c55d99ef8b6919221a52f7222625
Bárbara <babivaldez@gmail.com>
2023-10-30 19:20:50 -0300

    ajusta o arquivo config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,17 @@
+// const config = {
+//   username: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE || 'projetogeneration_db',
+//   host: process.env.MYSQL_HOST,
+//   dialect: 'mysql',
+// };
+
 const config = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE || 'projetogeneration_db',
-  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE || 'projetogeneration_db',
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   dialect: 'mysql',
 };
 
