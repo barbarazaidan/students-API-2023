6715219bda7d9ad6a855d513f87bcd72cf07baa2
Bárbara <babivaldez@gmail.com>
2023-10-30 20:46:07 -0300

    ajusta configurações para deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,8 @@
-// const config = {
-//   username: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-//   database: process.env.MYSQL_DATABASE || 'projetogeneration_db',
-//   host: process.env.MYSQL_HOST,
-//   dialect: 'mysql',
-// };
-
-// module.exports = {
-//   development: config,
-//   test: config,
-//   production: config,
-// };
-
 const config = {
-  username: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '123456',
-  database: process.env.MYSQLDATABASE || 'projetogeneration_db',
-  host: process.env.MYSQLHOST || 'localhost',
-  port: process.env.MYSQLPORT || 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE || 'projetogeneration_db',
+  host: process.env.MYSQL_HOST,
   dialect: 'mysql',
 };
 
