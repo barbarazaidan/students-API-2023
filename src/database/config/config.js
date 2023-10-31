6a25decb20ca216cbe98cecc0a85aaceaf19ff9b
Bárbara <babivaldez@gmail.com>
2023-10-31 08:52:46 -0300

    ajusta o arquivo config.js e dockerfile.mysql

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ const config = {
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'root',
   database: process.env.MYSQL_DATABASE || 'projetogeneration_db',
-  host: process.env.MYSQL_HOST,
+  host: process.env.MYSQL_HOST || 'mysql-container',
   dialect: 'mysql',
 };
 
