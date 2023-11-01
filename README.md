77af92021ff792681c051d92f5062ab5ca791dd1
Bárbara <babivaldez@gmail.com>
2023-11-01 08:21:12 -0300

    ajusta o readme e o arquivo config.js

--- README.md
+++ README.md
@@ -7,9 +7,7 @@
 
 **2. Rode o comando npm run compose**
 
-**3. Rode o comando npm run migrate e, em seguida, o comando npm run seed**
-
-**4. Entre na url http://localhost:3001/api-docs/**
+**3. Entre na url http://localhost:3001/api-docs/**
 
 ## Caso precise visualizar os logs do container, basta clicar dar o comando docker logs id-container-api
 
--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,8 @@
 const config = {
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'root',
-  database: process.env.MYSQL_DATABASE || 'projetogeneration_db',
-  host: process.env.MYSQL_HOST || 'mysql-container',
+  username: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || 'root',
+  database: process.env.MYSQLDATABASE || 'projetogeneration_db',
+  host: process.env.MYSQLHOST || 'mysql-container',
   dialect: 'mysql',
 };
 
